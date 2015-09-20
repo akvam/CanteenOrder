cc9f57770a605fb872740d7bc481bcbaff5e5494
unknown <AKVAM@LT-738916.statoil.net>
2015-09-20 22:13:11 +0200

    test

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ var port = process.env.PORT || 13337;
 
 http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
+    res.end('Hello World here is today´s menu\n');
 }).listen(port);
